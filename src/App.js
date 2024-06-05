55a8f4735a9a38560f494435fa51a20f7c902587
TechPriestJon <13856887+TechPriestJon@users.noreply.github.com>
2024-06-05 16:31:46 -0700

    ref

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/"element={<Home />} />
-            <Route path="/" element={<About />} />
+            <Route path="/about" element={<About />} />
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
